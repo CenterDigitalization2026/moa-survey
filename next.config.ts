9fe58ebf8f7225dbc5473ff837cbb96eee01e84e
Behruz <aminov.heartzilla@gmail.com>
2026-09-15 11:53:08 +0500

    feat: configure allowed dev and server action origins in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "heartzilla",
+    "heartzilla.local",
+    "*.local",
+    "192.168.100.170",
+    "192.168.100.*",
+    "192.168.*.*",
+    "10.*.*.*",
+    "localhost",
+    "127.0.0.1",
+  ],
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "heartzilla:3000",
+        "heartzilla",
+        "heartzilla.local:3000",
+        "heartzilla.local",
+        "*.local",
+        "*.local:3000",
+        "192.168.100.170:3000",
+        "192.168.100.170",
+        "192.168.100.*:3000",
+        "192.168.100.*",
+        "192.168.*.*:3000",
+        "192.168.*.*",
+        "10.*.*.*:3000",
+        "10.*.*.*",
+        "localhost:3000",
+        "localhost",
+        "127.0.0.1:3000",
+        "127.0.0.1",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
